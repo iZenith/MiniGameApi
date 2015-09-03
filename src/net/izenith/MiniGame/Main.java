ef023691d749a530a40147467956014b16a89b1a
Will Garrett <willgarrett610@gmail.com>
2015-09-03 17:47:10 -0600

    Copy files

---
+++ src/net/izenith/MiniGame/Main.java
@@ -0,0 +1,15 @@
+package net.izenith.MiniGame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
